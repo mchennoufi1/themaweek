2ade5b6f48a84cc842ee27291b8bd274af884b58
mchennoufi1 <70711395+mchennoufi1@users.noreply.github.com>
2023-02-05 15:05:28 +0100

    nice

---
+++ OOPHP/Person.php
@@ -0,0 +1,42 @@
+<?php
+{
+    class Person
+    {
+        public $naam;
+        private $leeftijd;
+        protected $geslacht;
+        public $isStudent;
+        public $gemiddeldCijfer;
+        function __construct(string $naam,
+        int $leeftijd, string $geslacht)
+        {
+        $this->naam = $naam;
+        $this->leeftijd = $leeftijd;
+        $this->geslacht = $geslacht;
+        echo "\n Nieuw Persoon-object aangemaakt";
+        echo "\n De property naam is " . $this->naam;
+        }
+        function __destruct()
+        {
+            //voerhierdebenodigdeactiesuit;
+            echo "\nPersoon object $this->naam wordt verwijderd";
+        }
+
+        function setGeslacht(string $geslacht)
+        {
+            $this->geslacht = $geslacht;
+        }
+        
+        function getGeslacht()
+        {
+            return $this->geslacht;
+        }
+        function getGegevens(){
+            $gegevens = 
+            "\n De gegevens van " . $this->naam . "zijn: " . 
+            "\nteeftijd: " . $this->leeftijd . 
+            "\nGeslacht: " . $this->geslacht;
+            return $gegevens;
+        }
+}
+}
