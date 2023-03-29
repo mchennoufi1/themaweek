e5ed025bd8631ec245f3d2a1e8d2cdf11a82349f
mchennoufi1 <70711395+mchennoufi1@users.noreply.github.com>
2023-03-29 16:03:17 +0200

    .

---
+++ OOPHP/basisonderdelen/Program.php
@@ -0,0 +1,12 @@
+<?php
+include 'Person.php';
+$umut = new Person("Umut", 18, "M");
+unset($umut);
+$demirel = new Person("Demirel", 19, "F");
+$mohammed = new Person("Mohammed", 19, "M");
+$thamara = new Person("Thamara", 18, "F");
+$jan = new Person("Jan", 21, "M");
+
+?>
+
+
