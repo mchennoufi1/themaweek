d7fa7859cfff7c4e5fd9472e6430769cfe11cd40
mchennoufi1 <70711395+mchennoufi1@users.noreply.github.com>
2023-03-29 15:58:50 +0200

    .

--- OOPHP/Person.php
+++ OOPHP/Person.php
@@ -1,42 +1,86 @@
 <?php
-{
-    class Person
-    {
-        public $naam;
-        private $leeftijd;
-        protected $geslacht;
-        public $isStudent;
-        public $gemiddeldCijfer;
-        function __construct(string $naam,
-        int $leeftijd, string $geslacht)
-        {
-        $this->naam = $naam;
-        $this->leeftijd = $leeftijd;
-        $this->geslacht = $geslacht;
-        echo "\n Nieuw Persoon-object aangemaakt";
-        echo "\n De property naam is " . $this->naam;
-        }
-        function __destruct()
-        {
-            //voerhierdebenodigdeactiesuit;
-            echo "\nPersoon object $this->naam wordt verwijderd";
-        }
 
-        function setGeslacht(string $geslacht)
-        {
-            $this->geslacht = $geslacht;
-        }
-        
-        function getGeslacht()
-        {
-            return $this->geslacht;
-        }
-        function getGegevens(){
-            $gegevens = 
-            "\n De gegevens van " . $this->naam . "zijn: " . 
-            "\nteeftijd: " . $this->leeftijd . 
-            "\nGeslacht: " . $this->geslacht;
-            return $gegevens;
-        }
-}
+    
+class Person
+{
+    public $name;
+    private $age;
+    protected $gender;
+    public $isStudent;
+    public $averageGrade;
+
+    function __construct(string $name, int $age, string $gender)
+    {
+        $this->name = $name;
+        $this->age = $age;
+        $this->gender = $gender;
+        echo "\n Nieuw Persoon-object aangemaakt";
+        echo "\n De property naam is " . $this->name;
+    }
+
+    function __destruct()
+    {
+        echo "\nPersoon object $this->name wordt verwijderd";
+    }
+
+    function setGender(string $gender)
+    {
+        $this->gender = $gender;
+    }
+
+    function getGender()
+    {
+        return $this->gender;
+    }
+
+    function setName(string $name)
+    {
+        $this->name = $name;
+    }
+
+    function getName()
+    {
+        return $this->name;
+    }
+
+    function setAge(int $age)
+    {
+        $this->age = $age;
+    }
+
+    function getAge()
+    {
+        return $this->age;
+    }
+
+    function setIsStudent(bool $value)
+    {
+        $this->isStudent = $value;
+    }
+
+    function getIsStudent()
+    {
+        return $this->isStudent;
+    }
+
+    function setAverageGrade(float $grade)
+    {
+        $this->averageGrade = $grade;
+    }
+
+    function getAverageGrade()
+    {
+        return $this->averageGrade;
+    }
+
+    function getGegevens()
+    {
+        $gegevens =
+            "\n De gegevens van " . $this->name . "zijn: " .
+            "\nLeeftijd: " . $this->age .
+            "\nGeslacht: " . $this->gender .
+            "\nIs student: " . ($this->isStudent ? "Ja" : "Nee") .
+            "\nGemiddeld cijfer: " . $this->averageGrade;
+        return $gegevens;
+    }
 }
--- OOPHP/Program.php
+++ OOPHP/Program.php
@@ -1,10 +1,35 @@
 <?php
 include 'Person.php';
+
 $umut = new Person("Umut", 18, "M");
-unset($umut);
+$umut->setIsStudent(true);
+$umut->setAverageGrade(7.5);
+echo $umut->getGegevens();
+
 $demirel = new Person("Demirel", 19, "F");
+$demirel->setIsStudent(true);
+$demirel->setAverageGrade(8.2);
+echo $demirel->getGegevens();
+
 $mohammed = new Person("Mohammed", 19, "M");
+$mohammed->setIsStudent(true);
+$mohammed->setAverageGrade(7.2);
+echo $mohammed->getGegevens();
+
 $thamara = new Person("Thamara", 18, "F");
+$thamara->setIsStudent(true);
+$thamara->setAverageGrade(7.2);
+echo $thamara->getGegevens();
+
 $jan = new Person("Jan", 21, "M");
+$jan->setIsStudent(true);
+$jan->setAverageGrade(7.2);
+echo $jan->getGegevens();
+
+$tim = new Person("Tim", 26, "M");
+$tim->setIsStudent(true);
+$tim->setAverageGrade(7.2);
+echo $tim->getGegevens();
 
 ?>
+
